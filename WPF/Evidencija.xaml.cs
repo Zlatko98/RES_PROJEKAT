8d7ef740c5977c64c5856f5b4b424f8cd33b3b27
Zlatko98 <zlatkolukic998@gmail.com>
2021-09-03 23:33:06 +0200

    Added new page (Evidencija)

---
+++ WPF/Evidencija.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for Evidencija.xaml
+    /// </summary>
+    public partial class Evidencija : Page
+    {
+        public Evidencija()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            MainPage page = new MainPage();
+            NavigationService.Navigate(page);
+        }
+
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+            parentWindow.Close();
+        }
+    }
+}
