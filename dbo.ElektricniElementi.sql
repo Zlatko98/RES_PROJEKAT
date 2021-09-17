bf730007eb34c00ca47bbe3867aefe216cb64d28
Zlatko98 <zlatkolukic998@gmail.com>
2021-09-17 15:27:23 +0200

    Added database

---
+++ dbo.ElektricniElementi.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ElektricniElementi] (
+    [idElement]    INT          NOT NULL,
+    [naziv]        VARCHAR (50) NULL,
+    [lokacija]     VARCHAR (50) NULL,
+    [tip]          VARCHAR (50) NULL,
+    [yKoordinata]  FLOAT (53)   NULL,
+    [xKoordinata]  FLOAT (53)   NULL,
+    [naponskiNivo] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([idElement] ASC)
+);
---
+++ dbo.Kvarovi.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Kvarovi] (
+    [Id]                   VARCHAR (50)  NOT NULL,
+    [datumKvara]           DATETIME      NULL,
+    [status]               VARCHAR (50)  NULL,
+    [datumZatvaranjaKvara] DATETIME      NULL,
+    [kratakOpis]           VARCHAR (50)  NULL,
+    [uzrok]                VARCHAR (MAX) NULL,
+    [detaljanOpis]         VARCHAR (MAX) NULL,
+    [opisAkcije]           VARCHAR (MAX) NULL,
+    [vremeAkcije]          DATETIME      NULL,
+    [elektricniElement]    VARCHAR (50)  NULL,
+    [idElement]            INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Kvarovi_ElektricniElementi] FOREIGN KEY ([idElement]) REFERENCES [dbo].[ElektricniElementi] ([idElement])
+);
