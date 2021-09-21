9dbdcb1f0a981d37ea197d13e71a454f0a95cb83
Zlatko98 <zlatkolukic998@gmail.com>
2021-09-21 15:30:18 +0200

    Added service class

---
+++ Servis/KvarServis.cs
@@ -0,0 +1,19 @@
+﻿using BazaPodataka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servis
+{
+    public class KvarServis
+    {
+        Baza db = new Baza();
+
+        public void unesiKvar(string kratakOpis, string uzrok, string detaljanOpis, DateTime vreme, string akcija, string elektricniElement)
+        {
+            db.DodajKvar(kratakOpis, uzrok, detaljanOpis, vreme, akcija, elektricniElement);
+        }
+    }
+}
