db7b8280a141d2069b809d1b510ae1ceaaed0ae3
Zlatko98 <zlatkolukic998@gmail.com>
2021-09-21 15:44:11 +0200

    Finishing service class

--- Servis/KvarServis.cs
+++ Servis/KvarServis.cs
@@ -1,4 +1,5 @@
 ﻿using BazaPodataka;
+using Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,22 @@ namespace Servis
         public void unesiKvar(string kratakOpis, string uzrok, string detaljanOpis, DateTime vreme, string akcija, string elektricniElement)
         {
             db.DodajKvar(kratakOpis, uzrok, detaljanOpis, vreme, akcija, elektricniElement);
+        }
+
+        public void azurirajKvar(string id, string kratakOpis, string uzrok, string detaljanOpis, Status status, DateTime vreme, string akcija, string elektricniElement)
+        {
+            db.AzurirajKvar(id, kratakOpis, uzrok, detaljanOpis, status, vreme, akcija, elektricniElement);
+        }
+
+        public List<ElektricniElement> GetElektricniElementi()
+        {
+            return db.GetElektricniElementi();
+        }
+
+
+        public List<Kvar> GetKvarovi(DateTime date1, DateTime date2)
+        {
+            return db.GetKvarovi(date1, date2);
         }
     }
 }
