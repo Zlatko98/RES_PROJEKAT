1883d429b212b828a5425e9863cfa91214d523a2
markotadic1999 <89256984+markotadic1999@users.noreply.github.com>
2021-09-24 18:31:03 +0200

    ListaKvarova.cs

--- WPF/ListaKvarova.xaml.cs
+++ WPF/ListaKvarova.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Common;
+using Servis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,58 @@ namespace WPF
     /// </summary>
     public partial class ListaKvarova : Page
     {
+        IKvar kvarServis = Factory.CreateKvarServis();
+        IExport exportServis = Factory.CreateExportServis();
         public ListaKvarova()
         {
             InitializeComponent();
+        }
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            MainPage page = new MainPage();
+            NavigationService.Navigate(page);
+        }
+
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            var window = (MainWindow)Application.Current.MainWindow;
+
+            window.Close();
+        }
+
+        private void Button_Click_Export_W(object sender, RoutedEventArgs e)
+        {
+            Kvar kvar = new Kvar();
+
+            kvar = (Kvar)ListBoxKvarovi.SelectedItem;
+
+            exportServis.ExportToWord(kvar);
+
+        }
+
+        private void Button_Click_Export_Ex(object sender, RoutedEventArgs e)
+        {
+            List<Kvar> list = new List<Kvar>();
+
+            foreach (var item in ListBoxKvarovi.Items)
+            {
+                list.Add((Kvar)item);
+            }
+
+            exportServis.ExportToExcel(list);
+        }
+        
+
+        private void Button_Click_Calculate(object sender, RoutedEventArgs e)
+        {
+            ListBoxKvarovi.ItemsSource = null;
+
+            DateTime date1 = (DateTime)DPicker1.SelectedDate;
+            DateTime date2 = (DateTime)DPicker2.SelectedDate;
+
+            ListBoxKvarovi.ItemsSource = kvarServis.GetKvarovi(date1, date2);
+            ListBoxKvarovi.DisplayMemberPath = "FullInfo";
+
         }
     }
 }
