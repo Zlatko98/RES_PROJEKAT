6b81be72ce5d978c46217e4d93d3666fc8e4a1f9
gnip-a <gnip.pr36.2017@uns.ac.rs>
2021-09-26 10:19:45 +0200

    Implementacija UnosKvara

--- WPF/UnosKvara.xaml.cs
+++ WPF/UnosKvara.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Common;
+using Servis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,52 @@ namespace WPF
     /// </summary>
     public partial class UnosKvara : Page
     {
+        IKvar kvarServis = Factory.CreateKvarServis();
+
         public UnosKvara()
         {
             InitializeComponent();
+            DataContext = this;
+            elementBox.ItemsSource = kvarServis.GetElektricniElementi();
+            elementBox.DisplayMemberPath = "GetIDnaziv";
+        }
+
+        private void Button_Click_DodajKvar(object sender, RoutedEventArgs e)
+        {
+            bool res = validation();
+
+            if (res)
+            {
+                string kratakOpis = kratakOpisBox.Text;
+                DateTime vreme = (DateTime)dateBox.SelectedDate;
+                string akcija = akcijaBox.Text;
+                string uzrok = uzrokBox.Text;
+                string element = elementBox.Text;
+                string opis = opisBox.Text;
+
+                kvarServis.unesiKvar(kratakOpis, uzrok, opis, vreme, akcija, element);
+            }
+        }
+
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            MainPage page = new MainPage();
+            NavigationService.Navigate(page);
+        }
+
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+            parentWindow.Close();
+        }
+
+
+
+        private bool validation()
+        {
+            bool result = true;
+
+            return result;
         }
     }
 }
